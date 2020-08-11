bc48ae7f0dfc0f3d606a8bc93a723f0758c52de2
segitiga <rafi.nizar15@gmail.com>
2020-08-11 22:44:13 +0700

    added: roll and flip

---
+++ cogs/gamble.py
@@ -0,0 +1,25 @@
+import discord
+from discord.ext import commands
+import random
+
+class Gamble(commands.Cog):
+
+    def __init__(self, client):
+        self.client = client
+    
+    @commands.command()
+    async def flip(self, ctx):
+        coin = ['Burung', 'Angka']
+        author = ctx.message.author
+        message = f'{author.mention} flipped [{random.choice(coin)}]'
+        await ctx.send(message)
+    
+    @commands.command()
+    async def roll(self, ctx, low_= 0, high_ = 100):
+        author = ctx.message.author
+        message = f'{author.mention} rolled [{random.randint(low_, high_)}]'
+        await ctx.send(message)
+
+
+def setup(client):
+    client.add_cog(Gamble(client))
